b338d2977d3d6eb024080254d3ab09cda790392a
devMay <duf1qudtls2@gmail.com>
2024-09-17 14:22:27 +0900

    example fix

---
+++ item-service/src/main/java/hello/itemservice/domain/information/InformationEntity.java
@@ -0,0 +1,9 @@
+package hello.itemservice.domain.information;
+
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
+
+public class InformationEntity {
+}
